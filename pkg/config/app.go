92cebd16f5b8ac06eefb7f678c2c0fbd72396df8
prashantrewar <prashant09032001@gmail.com>
2023-06-03 23:41:40 +0530

    First release of bookstore-api

---
+++ pkg/config/app.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:706288@/SimpleRest?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println("this", err)
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
